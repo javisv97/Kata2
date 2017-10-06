d5cb9d3857391eaf926b212898119736da1a3c19
usuario <usuario@10.22.148.52>
2017-10-06 11:49:01 +0100

    Kata2 version 3: usamos una clase histogram a la que le pasamos los datos

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -6,15 +6,14 @@ import java.util.Map;
 public class Kata2 {
     
     public static void main(String[] args) {
-        
-        int [] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-        Map<Integer, Integer> histograma = new HashMap<>();
        
-        for (int key : data) {
-            histograma.put(key, histograma.containsKey(key) ? histograma.get(key) + 1 :1 );
-        }
-        for(int key: histograma.keySet()){
-            System.out.println(key + " ==> " + histograma.get(key));
+       int [] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+       
+       Histogram histo = new Histogram(data);
+       Map<Integer, Integer> histogr = histo.getHistogram();
+        
+        for(int key: histogr.keySet()){
+            System.out.println(key + " ==> " + histogr.get(key));
         }
     }
 }
